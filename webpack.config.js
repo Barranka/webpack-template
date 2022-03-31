b354c111a4a5b5878c2ae3d12bc3ef2784d5fe77
Екатерина Хрипунова <cat123_93@mail.ru>
2022-03-31 19:08:39 +0300

    second commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,86 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
+module.exports = (env, argv) => {
+  const isProd = argv.mode === 'production';
+  const isDev = !isProd;
+  const filename = (ext) =>
+    isProd ? `[name].[contenthash].bundle.${ext}` : `[name].bundle.${ext}`;
 
-module.exports = {
+  const plugins = () => {
+    const base = [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+      }),
+      new FaviconsWebpackPlugin({
+        logo: './favicon.png',
+        cache: true,
+      }),
+      new MiniCssExtractPlugin({
+        filename: filename('css'),
+      }),
+    ];
+
+    if (isDev) {
+      base.push(new ESLintPlugin());
+    }
+
+    return base;
+  };
+
+  return {
     context: path.resolve(__dirname, 'src'),
     entry: {
-        main: './index.js'
+      main: [
+        'core-js/stable',
+        'regenerator-runtime/runtime',
+        './index.js',
+      ],
     },
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      filename: filename('js'),
+      clean: true,
     },
-    devtool: false,
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './index.html',
-        })
-    ],
-}
+    resolve: {
+      extensions: ['.js'],
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+        '@core': path.resolve(__dirname, 'src', 'core'),
+      },
+    },
+    devServer: {
+      port: 3000,
+      open: true,
+      hot: true,
+      watchFiles: './',
+    },
+    plugins: plugins(),
+    devtool: isDev ? 'source-map' : false,
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            'sass-loader',
+          ],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
+      ],
+    },
+  };
+};
